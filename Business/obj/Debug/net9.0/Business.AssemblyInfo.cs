3d91766b2f9bb89cebf11eb364c0393612044af3
Shashi <shashi@myhealthcare.life>
2025-12-13 23:08:22 +0530

    delete class1

--- Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b184eb66eaa89dedf1a685b2537b393ededced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fbdd8a8858c401b77794855b7ad29ba8006e538")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
